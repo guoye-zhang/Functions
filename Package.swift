6416d25a5d8f54116a217acadaf0692843505861
Guoye Zhang <guoye@d0g.cc>
2017-11-07 10:07:31 -0500

    Support SPM

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Functions",
+    targets: [
+        .target(
+            name: "funcgen",
+            path: "Functions"),
+    ]
+)
